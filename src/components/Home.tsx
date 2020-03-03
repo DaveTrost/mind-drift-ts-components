5cc5bc68e8bbbca98076a043c9e2de93def1aaac
Dave Trost <davetrost@gmail.com>
2020-03-03 15:19:47 -0800

    home component in TS with snapshot testing
    Still need to migrate the AchievementToast component to TS

---
+++ src/components/Home.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+import NavBar from './NavBar';
+// import AchievementToast from './AchievementToast';
+import mindDriftIcon from '../assets/mindDriftIcon.png';
+
+const Home = () => (
+  <section className="Home">
+    <NavBar />
+    {/* <AchievementToast /> */}
+    <div className="homeContainer">
+      <div className="logo">
+        <img src={mindDriftIcon} alt="mindDrift logo" />
+        <section className="headerText">
+          <h2>mind</h2>
+          <h3>Drift</h3>
+        </section>
+      </div>
+      <div className="linksContainer">
+        <Link to="/breathe">
+          <button className="breatheButton">Breathe</button>
+        </Link>
+        <Link to="/settings">
+          <button className="setupButton">Setup</button>
+        </Link>
+      </div>
+    </div>
+  </section>
+);
+
+export default Home;
