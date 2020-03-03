78385ba5c0b650c434f62d0821bf8c9e4c701c56
Dave Trost <davetrost@gmail.com>
2020-03-03 15:20:20 -0800

    App component converted to TS with snapshot testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// import withSessionMock from '../utils/WithSession';
+// jest.mock('../utils/WithSession', () => ({}));
+
+describe('App component', () => {
+  it('renders App in logged-in state', () => {
+    // withSessionMock.withSession = Comp => (<Comp />);
+    
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  // it('renders App when not logged in', () => {
+  //   withSessionMock.withSession = Comp => (<></>);
+
+  //   const wrapper = shallow(<App />);
+  //   expect(wrapper).toMatchSnapshot();
+  // });
 });
