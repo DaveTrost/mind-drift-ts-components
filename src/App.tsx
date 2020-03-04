6fadeaff486cf8630932d3fcee511c23127c3fb0
Dave Trost <davetrost@gmail.com>
2020-03-04 09:13:59 -0800

    CSS classname collides with bootstrap class, rename it

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.scss';
 
 function App() {
   return (
-    <div className={"container"}>
+    <div className={"appContainer"}>
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
